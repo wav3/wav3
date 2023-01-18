ef359c4c4aa4caaf87058cbfe60259e5d55b3dc2
benmelz <ben@melz.me>
2023-01-18 15:01:41 -0500

    chore: test ci

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,6 +18,11 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
+
+  config.before :suite do
+    system 'yarn build', out: File::NULL
+    system 'yarn build:css', out: File::NULL
+  end
 end
 
 Shoulda::Matchers.configure do |config|
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start('rails') { enable_coverage :branch }
+
+SimpleCov.start('rails') do
+  enable_coverage :branch
+  minimum_coverage line: 100, branch: 100
+end
 
 RSpec.configure do |config|
   config.expect_with(:rspec) { |expectations| expectations.include_chain_clauses_in_custom_matcher_descriptions = true }
