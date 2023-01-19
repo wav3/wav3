89b4612ab86ca82415e4edd46d0321d75e76e2fe
benmelz <ben@melz.me>
2023-01-18 20:19:26 -0500

    chore: remove redundant self

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,6 @@ module Wav3
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    config.exceptions_app = self.routes
+    config.exceptions_app = routes
   end
 end
