ed4e6f3b1c9cf0ed0b01f7945066ffc59377a32e
benmelz <ben@melz.me>
2023-01-24 17:04:03 -0500

    chore: user model

--- spec/models/application_record_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe ApplicationRecord do
-  pending 'is an abstract class' # TODO: Remove file once a real model spec is added.
-end
