5dfd5f35bcc6f0b4bcb3f95ee5f163424fa9addb
benmelz <ben@melz.me>
2023-01-24 18:03:56 -0500

    feat: flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  add_flash_types :primary, :secondary, :success, :danger, :warning, :info, :light, :dark
+
   def root; end
 end
