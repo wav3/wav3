b46e7eec5e02a7eae3b8a37f812647e6ca6bf8ed
Victor Santana <victor.santana@cimpress.com>
2023-01-25 08:36:43 -0500

    Fixed Test Linting Issue

--- spec/root_spec.rb
+++ spec/root_spec.rb
@@ -1,8 +1,9 @@
-# spec/home_spec.rb
+# frozen_string_literal: true
+# spec/root_spec.rb
 
 require 'rails_helper'
 
-RSpec.describe 'Home content' do
+RSpec.describe 'Root content' do
   it 'shows the app name' do
     visit root_path
     expect(page).to have_content('WAV3')
