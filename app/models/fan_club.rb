a0885aa9f485bf41fb51ae5942902d6a8690c151
Victor Santana <victor.santana@cimpress.com>
2023-01-26 15:48:50 -0500

    Revert "Bug: Admin NameError"
    This reverts commit bcb1fbb2fac2231836f982cf587f3b616712d987.

--- app/models/fan_club.rb
+++
@@ -1,3 +0,0 @@
-class FanClub < ApplicationRecord
-  resourcify
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  rolify :role_cname => 'Admin'
   devise :database_authenticatable, :validatable
 
   has_one :profile, dependent: :destroy, required: true, validate: true
