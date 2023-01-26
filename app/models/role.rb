a3fc82233030f3e1631cd0bb002423ffd9895ff4
Victor Santana <victor.santana@cimpress.com>
2023-01-26 17:00:35 -0500

    spec: removed lingering rolify validation

--- app/models/role.rb
+++ app/models/role.rb
@@ -6,7 +6,5 @@ class Role < ApplicationRecord
 
   belongs_to :resource, polymorphic: true, optional: true
 
-  validates :resource_type, inclusion: { in: Rolify.resource_types }, allow_nil: true
-
   scopify
 end
